a48a8cebf3c65a4065880c04abd3cb01b1ee1028
Yurpolskiy <fpbidni@gmail.com>
2022-09-23 20:47:15 +0300

    done latest-news section :D

--- js/script.js
+++ js/script.js
@@ -10,4 +10,30 @@ let slick = $(document).ready(function() {
         appendDots: $('.dots')
     })
 })
-let flag = false;
+let slider = $(document).ready(function(){
+    $('.latest-news__slider').slick({
+        arrows: true,
+        dots: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        adaptiveHeight: true,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        appendDots: $('.latest-news__slider_dots'),
+        responsive: [
+            {
+                breakpoint: 1350,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+            breakpoint: 770,
+                settings: {
+                slidesToShow: 1
+                }
+        }
+        ]
+    })
+})
