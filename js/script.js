154ab928ee5d14378f98b23155863954ddf2e1ce
Yurpolskiy <fpbidni@gmail.com>
2022-09-26 00:21:21 +0300

    Sketched the login form

--- js/script.js
+++ js/script.js
@@ -37,3 +37,14 @@ let slider = $(document).ready(function(){
         ]
     })
 })
+$('.scroll').on( 'click', function(){
+    var el = $(this);
+    var dest = el.attr('href'); // получаем направление
+    if(dest !== undefined && dest !== '') { // проверяем существование
+        $('html').animate({
+                scrollTop: $(dest).offset().top // прокручиваем страницу к требуемому элементу
+            }, 500 // скорость прокрутки
+        );
+    }
+    return false;
+});
